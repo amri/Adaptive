3c239e3032eacb2f98e72e8e6715a1999aa4467d
amri.hidayat <amri.hidayat@gmail.com>
2014-12-03 20:30:09 +0800

    Include a simple TDD & refactoring project

---
+++ Adaptive/UnitTesting/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTesting
+{
+    public class Account
+    {
+        public void AddTransaction(decimal amountDecimal)
+        {
+            this.Balance += amountDecimal;
+        }
+
+        public decimal Balance { get; internal set; }
+    }
+}
